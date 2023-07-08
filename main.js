32d6a613218032d3b857f7cd997975675a6bfff8
ercrrc <134560333+ercrrc@users.noreply.github.com>
2023-07-08 18:42:41 -0300

    agenda de contato

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-function verificaSeNumeroBeMaiorqueNumeroA(){
-    const valorA = document.getElementById('valor-a').value;
-    const valorB = document.getElementById('valor-b').value;
-    
-    if(valorB > valorA) {
-        alert('valor do campo B e maior que do campo A');
-    } else {
-        alert('valor do campo A e maior que do campo B');
-    }
+const form = document.getElementById('form-cadastro');
+let linhas = '';
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
 }
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeCompleto = document.getElementById('nome-completo');
+    const inputContato = document.getElementById('contato');
+    
+
+    let linha = `<tr>`;
+    linha += `<td>${inputNomeCompleto.value}</td>`;
+    linha += `<td>${inputContato.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeCompleto.value = '';
+    inputContato.value = '';
+});
+
 
 
 
